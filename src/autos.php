47226100eddbf0d25f93e6947206784ef0c8fe13
DiMoNiO04 <dima.razumov.940@mail.ru>
2023-03-27 21:14:23 +0300

    refactor: autos css and html page

--- src/autos.php
+++ src/autos.php
@@ -15,25 +15,23 @@
 			<div class="header__container">
 				<nav>
 					<ul class="header__list">
-						<li class="header__item"><a href="./autos.php">Автомобили</a></li>
-						<li class="header__item"><a href="./about.php">О компании</a></li>
-						<li class="header__item"><a href="./service.php">Услуги</a></li>
-						<li class="header__item"><a href="./contacts.php">Контакты</a></li>
+						<li class="header__item"><a class="header__item-link" href="autos.php">Автомобили</a></li>
+						<li class="header__item"><a class="header__item-link" href="about.php">О компании</a></li>
+						<li class="header__item"><a class="header__item-link" href="service.php">Услуги</a></li>
+						<li class="header__item"><a class="header__item-link" href="contacts.php">Контакты</a></li>
 					</ul>
 				</nav>
 				<div class="header__active">
-					<ul>
-						<li  class="header__cab">
-							<a href="#"><i class="fa-solid fa-user"></i>Кабинет</a>
-							<ul class="header__logout">
-								<li class="header__item"><a href="./auth.php">Вход</a></li>
-								<li class="header__item"><a href="./reg.php">Регистрация</a></li>
-								<li class="header__item"><a href="./personal__cab-user.php">Личный кабинет</a></li>
-								<li class="header__item"><a href="./admin__panel.php">Админ панель</a></li>
-							</ul>
-						</li>
-					</ul>
-					<a href="index.php">
+					<div class="header__cab">
+						<button class="header__cab-button" href="#"><i class="fa-solid fa-user"></i>Кабинет</button>
+						<ul class="header__logout">
+							<li class="header__item"><a class="header__logout-link" href="auth.php">Вход</a></li>
+							<li class="header__item"><a class="header__logout-link" href="reg.php">Регистрация</a></li>
+							<li class="header__item"><a class="header__logout-link" href="personal__cab-user.php">Личный кабинет</a></li>
+							<li class="header__item"><a class="header__logout-link" href="admin__panel.php">Админ панель</a></li>
+						</ul>
+					</div>
+					<a href="index.php" class="logo-bmw">
 						<img src="./front/images/dest/svg/bmw_logo.svg" alt="Logo_BMW">
 					</a>
 				</div>
@@ -62,9 +60,9 @@
 					<div class="auto__filtr">
 						<form method="post" action="" class="form__search">
 							<h2 class="search__title">Подберите авто по вашим запросам</h2>
-							<div class="search__container">
+							<ul class="search__container">
 
-								<div class="search__block">
+								<li class="search__block">
 									<h3>Модель</h3>
 									<select name="model" class="search__select">
 										<option selected>Модель:</option>
@@ -72,9 +70,9 @@
 										<option value="4">4</option>
 										<option value="3">3</option>
 									</select>
-								</div>
+								</li>
 
-								<div class="search__block">
+								<li class="search__block">
 									<h3>Цвет</h3>
 									<select name="color" class="search__select">
 										<option selected>Цвет:</option>
@@ -84,10 +82,9 @@
 										<option value="white">Белый</option>
 										<option value="black">Черный</option>
 									</select>
-								</div>
+								</li>
 
-								
-								<div class="search__block">
+								<li class="search__block">
 									<h3>Наличие</h3>
 									<label class="custom-checkbox">
 										<input name="available" type="checkbox" value="available">
@@ -97,9 +94,9 @@
 										<input name="no_available" type="checkbox" value="no_available">
 										<span>Нету</span>
 									</label>
-								</div>
+								</li>
 
-								<div class="search__block">
+								<li class="search__block">
 									<h3>Двигатель</h3>
 									<label class="custom-checkbox">
 										<input name="engine" type="checkbox" value="patrol">
@@ -109,9 +106,9 @@
 										<input name="engine" type="checkbox" value="electric">
 										<span>Электрический</span>
 									</label>
-								</div>
+								</li>
 
-								<div class="search__block">
+								<li class="search__block">
 									<h3>Коробка передач</h3>
 									<label class="custom-checkbox">
 										<input name="automatic" type="checkbox" value="automatic">
@@ -121,40 +118,40 @@
 										<input name="manual" type="checkbox" value="manual">
 										<span>Ручная</span>
 									</label>
-								</div>
+								</li>
 
-								<div class="search__block">
+								<li class="search__block">
 									<h3>Год выпуска</h3>
 									<input type="number" min="2000" max="2023" name="year__from" id="year__from" class="search__input" placeholder="От">
 									<input type="number" min="2000" max="2023" name="year__to" id="year__to" class="search__input" placeholder="До">
-								</div>
+								</li>
 
-								<div class="search__block">
+								<li class="search__block">
 									<h3>Цена</h3>
 									<input type="number" min="30000" step="5000" name="price__from" id="price__from" class="search__input" placeholder="От">
 									<input type="number" min="30000" step="5000" name="price__to" id="price__to" class="search__input" placeholder="До">
-								</div>
-							</div>
+								</li>
+							</ul>
 							<button class="button button__search" name="search__auto" type="submit" title="Найти">Найти</button>
 						</form>
 					</div>
 
 					<div class="auto__sidebar">
-						<div class="sidebar__list">
-							<a href="#" class="sidebar__item sidebar__item-active" id="0">ALL</a>
-							<a href="#" class="sidebar__item" id="1">M</a>
-							<a href="#" class="sidebar__item" id="2">X</a>
-							<a href="#" class="sidebar__item" id="3">I</a>
-							<a href="#" class="sidebar__item" id="4">8</a>
-							<a href="#" class="sidebar__item" id="5">7</a>
-							<a href="#" class="sidebar__item" id="6">6</a>
-							<a href="#" class="sidebar__item" id="7">5</a>
-							<a href="#" class="sidebar__item" id="8">4</a>
-							<a href="#" class="sidebar__item" id="9">3</a>
-							<a href="#" class="sidebar__item" id="10">2</a>
-							<a href="#"  class="sidebar__item" id="11">Z4</a>
-							<a href="#" class="sidebar__item" id="12">Концепты</a>
-						</div>
+						<ul class="sidebar__list">
+							<li><a href="#" class="sidebar__item sidebar__item-active" id="0">ALL</a></li>
+							<li><a href="#" class="sidebar__item" id="1">M</a></li>
+							<li><a href="#" class="sidebar__item" id="2">X</a></li>
+							<li><a href="#" class="sidebar__item" id="3">I</a></li>
+							<li><a href="#" class="sidebar__item" id="4">8</a></li>
+							<li><a href="#" class="sidebar__item" id="5">7</a></li>
+							<li><a href="#" class="sidebar__item" id="6">6</a></li>
+							<li><a href="#" class="sidebar__item" id="7">5</a></li>
+							<li><a href="#" class="sidebar__item" id="8">4</a></li>
+							<li><a href="#" class="sidebar__item" id="9">3</a></li>
+							<li><a href="#" class="sidebar__item" id="10">2</a></li>
+							<li><a href="#" class="sidebar__item" id="11">Z4</a></li>
+							<li><a href="#" class="sidebar__item" id="12">Концепты</a></li>
+						</ul>
 					</div>
 
 					<div class="auto__content">
@@ -258,28 +255,28 @@
 		<div class="container">
 			<div class="footer__container">
 				<div class="footer__content">
-					<a href="./index.php" class="footer__logo-bmw">
+					<a href="index.php" class="logo-bmw">
 						<img src="./front/images/dest/svg/bmw_logo.svg" alt="bmw-logo">
 					</a>
 					<ul class="footer__items">
-						<li class="footer__item"><a href="./autos.php">Автомобили</a></li>
-						<li class="footer__item"><a href="./about.php">О компании</a></li>
-						<li class="footer__item"><a href="./service.php">Услуги и сервис</a></li>
-						<li class="footer__item"><a href="./contacts.php">Контакты</a></li>
+						<li><a class="footer__items-link" href="./autos.php">Автомобили</a></li>
+						<li><a class="footer__items-link" href="./about.php">О компании</a></li>
+						<li><a class="footer__items-link" href="./service.php">Услуги и сервис</a></li>
+						<li><a class="footer__items-link" href="./contacts.php">Контакты</a></li>
 					</ul>
 					<div class="footer__socials">
-						<a href="https://vk.com/bmw" target="_blank" class="social__icon"><i class="fa-brands fa-vk"></i></a>
-						<a href="https://www.youtube.com/user/BMWinRussia" target="_blank" class="social__icon"><i
+						<a href="https://vk.com/bmw" target="_blank" class="footer__socials-icon"><i class="fa-brands fa-vk"></i></a>
+						<a href="https://www.youtube.com/user/BMWinRussia" target="_blank" class="footer__socials-icon"><i
 								class="fa-brands fa-youtube"></i></a>
-						<a href="https://t.me/bmw_russia_official" target="_blank" class="social__icon"><i
+						<a href="https://t.me/bmw_russia_official" target="_blank" class="footer__socials-icon"><i
 								class="fa-brands fa-telegram"></i></a>
-						<a href="https://www.tiktok.com/@bmwrussia?" target="_blank" class="social__icon"><i
+						<a href="https://www.tiktok.com/@bmwrussia?" target="_blank" class="footer__socials-icon"><i
 								class="fa-brands fa-tiktok"></i></a>
 					</div>
 				</div>
 				<div class="footer__author">
 					<p>Разработка сайта <span>© 2023</span></p>
-					<a href="mailto:dima.razumov.940@mail.ru" class="author__mail">dima.razumov.940@mail.ru</a>
+					<a href="mailto:dima.razumov.940@mail.ru" class="footer__author-mail">dima.razumov.940@mail.ru</a>
 				</div>
 			</div>
 		</div>
