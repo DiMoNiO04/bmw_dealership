7c3d1c463d7c45c4d91c8199c830529c0ae6c9ab
DiMoNiO04 <dima.razumov.940@mail.ru>
2023-03-27 19:30:49 +0300

    refactor: about css and html page

--- src/about.php
+++ src/about.php
@@ -15,25 +15,23 @@
 			<div class="header__container header-blue__container">
 				<nav>
 					<ul class="header__list">
-						<li class="header__item"><a href="./autos.php">Автомобили</a></li>
-						<li class="header__item"><a href="./about.php">О компании</a></li>
-						<li class="header__item"><a href="./service.php">Услуги</a></li>
-						<li class="header__item"><a href="./contacts.php">Контакты</a></li>
+						<li class="header__item"><a class="header__item-link" href="autos.php">Автомобили</a></li>
+						<li class="header__item"><a class="header__item-link" href="about.php">О компании</a></li>
+						<li class="header__item"><a class="header__item-link" href="service.php">Услуги</a></li>
+						<li class="header__item"><a class="header__item-link" href="contacts.php">Контакты</a></li>
 					</ul>
 				</nav>
 				<div class="header__active">
-					<ul>
-						<li class="header__cab">
-							<a href="#"><i class="fa-solid fa-user"></i>Кабинет</a>
-							<ul class="header__logout header-blue__logout">
-								<li class="header__item"><a href="./auth.php">Вход</a></li>
-								<li class="header__item"><a href="./reg.php">Регистрация</a></li>
-								<li class="header__item"><a href="./personal__cab-user.php">Личный кабинет</a></li>
-								<li class="header__item"><a href="./admin__panel.php">Админ панель</a></li>
-							</ul>
-						</li>
-					</ul>
-					<a href="index.php">
+					<div class="header__cab">
+						<button class="header__cab-button" href="#"><i class="fa-solid fa-user"></i>Кабинет</button>
+						<ul class="header__logout header-blue__logout">
+							<li class="header__item"><a class="header__logout-link" href="auth.php">Вход</a></li>
+							<li class="header__item"><a class="header__logout-link" href="reg.php">Регистрация</a></li>
+							<li class="header__item"><a class="header__logout-link" href="personal__cab-user.php">Личный кабинет</a></li>
+							<li class="header__item"><a class="header__logout-link" href="admin__panel.php">Админ панель</a></li>
+						</ul>
+					</div>
+					<a href="index.php" class="logo-bmw">
 						<img src="./front/images/dest/svg/bmw_logo.svg" alt="Logo_BMW">
 					</a>
 				</div>
@@ -77,20 +75,20 @@
 								комфортным и оперативным для Вас.</li>
 						</ul>
 					</div>
-					<div class="company__links">
-						<div class="company__link">
+					<ul class="company__links">
+						<li class="company__link">
 							<h2 class="link__title">Ознакомиться с услугами</h2>
-							<a class="button button__company" href="./service.php" title="Услуги">Подробнее</a>
-						</div>
-						<div class="company__link">
+							<a class="button company-button" href="./service.php" title="Услуги">Подробнее</a>
+						</li>
+						<li class="company__link">
 							<h2 class="link__title">Ознакомиться с контактами</h2>
-							<a class="button button__company" href="./contacts.php" title="Контакты">Подробнее</a>
-						</div>
-						<div class="company__link">
+							<a class="button company-button" href="./contacts.php" title="Контакты">Подробнее</a>
+						</li>
+						<li class="company__link">
 							<h2 class="link__title">Ознакомиться с автомобилимя</h2>
-							<a class="button button__company" href="./autos.php" title="Автомобили">Подробнее</a></button>
-						</div>
-					</div>
+							<a class="button company-button" href="./autos.php" title="Автомобили">Подробнее</a></button>
+						</li>
+					</ul>
 					<div class="company__data">
 						<h2 class="company__title">Данные компании</h2>
 						<p>Этот веб-сайт обслуживается Bayerische Motoren Werke Aktiengesellschaft (Petuelring 130, 80788
@@ -98,7 +96,7 @@
 						<p>Электронные контактные данные: <span>customer.service@bmw.com</span>.</p>
 						<p>Импортёр: UAB Autoimex (Žalgirio g. 112A, LT-09300 Vilnius. Lithuania).</p>
 						<p>Электронные контактные данные: <span>info@bmw.by</span></p>
-								<p>Код предприятия: 300662220</p>
+						<p>Код предприятия: 300662220</p>
 					</div>
 				</div>
 			</div>
@@ -109,28 +107,28 @@
 		<div class="container">
 			<div class="footer__container">
 				<div class="footer__content">
-					<a href="./index.html" class="footer__logo-bmw">
+					<a href="index.php" class="logo-bmw">
 						<img src="./front/images/dest/svg/bmw_logo.svg" alt="bmw-logo">
 					</a>
 					<ul class="footer__items">
-						<li class="footer__item"><a href="./autos.php">Автомобили</a></li>
-						<li class="footer__item"><a href="./about.php">О компании</a></li>
-						<li class="footer__item"><a href="./service.php">Услуги и сервис</a></li>
-						<li class="footer__item"><a href="./contacts.php">Контакты</a></li>
+						<li><a class="footer__items-link" href="./autos.php">Автомобили</a></li>
+						<li><a class="footer__items-link" href="./about.php">О компании</a></li>
+						<li><a class="footer__items-link" href="./service.php">Услуги и сервис</a></li>
+						<li><a class="footer__items-link" href="./contacts.php">Контакты</a></li>
 					</ul>
 					<div class="footer__socials">
-						<a href="https://vk.com/bmw" target="_blank" class="social__icon"><i class="fa-brands fa-vk"></i></a>
-						<a href="https://www.youtube.com/user/BMWinRussia" target="_blank" class="social__icon"><i
+						<a href="https://vk.com/bmw" target="_blank" class="footer__socials-icon"><i class="fa-brands fa-vk"></i></a>
+						<a href="https://www.youtube.com/user/BMWinRussia" target="_blank" class="footer__socials-icon"><i
 								class="fa-brands fa-youtube"></i></a>
-						<a href="https://t.me/bmw_russia_official" target="_blank" class="social__icon"><i
+						<a href="https://t.me/bmw_russia_official" target="_blank" class="footer__socials-icon"><i
 								class="fa-brands fa-telegram"></i></a>
-						<a href="https://www.tiktok.com/@bmwrussia?" target="_blank" class="social__icon"><i
+						<a href="https://www.tiktok.com/@bmwrussia?" target="_blank" class="footer__socials-icon"><i
 								class="fa-brands fa-tiktok"></i></a>
 					</div>
 				</div>
 				<div class="footer__author">
 					<p>Разработка сайта <span>© 2023</span></p>
-					<a href="mailto:dima.razumov.940@mail.ru" class="author__mail">dima.razumov.940@mail.ru</a>
+					<a href="mailto:dima.razumov.940@mail.ru" class="footer__author-mail">dima.razumov.940@mail.ru</a>
 				</div>
 			</div>
 		</div>
